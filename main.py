98d9c75d44292db6c34651fb5d75765f4ae6eb82
Ethan Cho <ethan.cho1826@gmail.com>
2022-07-19 14:18:46 -0500

    turtle stuff, draw a square, draw a dashed line

---
+++ main.py
@@ -0,0 +1,19 @@
+from turtle import Turtle, Screen
+
+the_turtle = Turtle()
+the_turtle.shape('turtle')
+the_turtle.color('blue')
+# draw a square
+# for i in range(4):
+#     the_turtle.forward(10)
+#     the_turtle.right(90)
+
+# draw dashed line
+# for i in range(15):
+#     the_turtle.forward(10)
+#     the_turtle.penup()
+#     the_turtle.forward(10)
+#     the_turtle.pendown()
+
+screen = Screen()
+screen.exitonclick()
