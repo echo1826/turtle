bf42da8e8ef9abd56367beffa673b47a4f90a75f
Ethan Cho <ethan.cho1826@gmail.com>
2022-07-19 14:28:22 -0500

    drawing different shapes all at once

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from turtle import Turtle, Screen
+import random
 
 the_turtle = Turtle()
 the_turtle.shape('turtle')
@@ -14,6 +15,21 @@ the_turtle.color('blue')
 #     the_turtle.penup()
 #     the_turtle.forward(10)
 #     the_turtle.pendown()
+
+# def draw_shape(num_side):
+#     angle = CIRCLE / num_sides
+#     for i in range(num_sides):
+#         the_turtle.forward(100)
+#         the_turtle.right(angle)        
+
+# colors = ["medium aquamarine", "gold", "red", "purple", "green", "blue", "black", "pink"]
+# num_sides = 3
+# CIRCLE = 360
+# for j in range(3, 11):
+#     the_turtle.color(random.choice(colors))
+#     draw_shape(num_sides)
+#     num_sides += 1
+
 
 screen = Screen()
 screen.exitonclick()
